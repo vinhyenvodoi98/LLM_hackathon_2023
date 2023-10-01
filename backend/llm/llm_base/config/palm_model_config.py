f51ef00c63d977d099cb6be1e065a295f5153a88
Nhi Nguyen <nvnhi1811@gmail.com>
2023-10-02 03:05:26 +0900

    Add config files for LLM model and prompt

---
+++ backend/llm/llm_base/config/palm_model_config.py
@@ -0,0 +1,31 @@
+text_model = {
+    "model_name": "models/text-bison-001",
+    "parameters": {
+        "temperature": 0.2,
+        "max_output_tokens": 1024,
+        "top_k": 40,
+        "top_p": 0.8
+    },
+}
+
+
+chat_model = {
+    "model_name": "models/chat-bison-001",
+    "parameters": {
+        "temperature": 0.2,
+        "max_output_tokens": 1024,
+        "top_k": 40,
+        "top_p": 0.8
+    },
+}
+
+
+embedding_model = {
+    "model_name": "models/embedding-gecko-002",
+    "parameters": {
+        "temperature": 0.2,
+        "max_output_tokens": 1024,
+        "top_k": 40,
+        "top_p": 0.8
+    },
+}
