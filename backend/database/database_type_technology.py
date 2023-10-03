a32045acda2082320048bb1a4cf9257aba06ecd2
Hà Việt Tiến <34891363+tienthegainz@users.noreply.github.com>
2023-10-03 23:35:26 +0900

    Fix database template and add db classify template #13 (#18)
    * #4: Add database
    
    * #4: Add more databases

--- backend/database/database_type_technology.py
+++ backend/database/database_type_technology.py
@@ -1,5 +1,28 @@
-# database type technologies of mock db
-db_database_type_technologies = [
-    # TODO: add data
-    # example: {"name": "MySQL", "data_type": "SQL, ...}
-]
+import json
+import os
+from pprint import pprint
+
+
+def remove_question_key(raw_db_attributes: dict) -> dict:
+    """Remove question key"""
+    db_attributes = {}
+    for key, attr in raw_db_attributes.items():
+        if not isinstance(attr, dict):
+            db_attributes[key] = attr
+        else:
+            db_attributes[key] = attr["answer"]
+    return db_attributes
+
+
+def load_database_type_technologies():
+    database_type_technologies_dir = "database_type_technologies/"
+    technologies = []
+    for file_name in os.listdir(database_type_technologies_dir):
+        file_path = os.path.join(database_type_technologies_dir, file_name)
+        with open(file_path) as f:
+            technologies.append(remove_question_key(json.load(f)))
+    technologies.sort(key=lambda tech: tech["index"])
+    return technologies
+
+
+db_database_type_technologies = load_database_type_technologies()
