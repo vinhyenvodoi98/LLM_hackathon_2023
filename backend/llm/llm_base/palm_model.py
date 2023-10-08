317331613c9fe2e505ebfda444cd96d998edfe03
Nhi.Nguyen <nhi.nguyen@access-company.com>
2023-10-08 15:06:51 +0900

    #14: Create LLM base and interactors
    * Add config files for LLM model and prompt
    
    * Add LLM base classes
    
    * Add functions for backend to interact with LLM
    
    * Refactor Prompt class for reusability
    
    * Add temporary parser to extract json from output
    
    * Move LLM required packges list to Backend's
    
    * Add new line at the end of file
    
    * Delete the old LLM's requirements file
    
    * Add new line again?
    
    * Minor changes
    
    * Minor changes again
    
    * Fix how new line is added at the end of file

---
+++ backend/llm/llm_base/palm_model.py
@@ -0,0 +1,36 @@
+import os
+from dotenv import load_dotenv
+from langchain.llms import GooglePalm
+from config.palm_model_config import * 
+
+load_dotenv()
+
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+
+class PalmModel:
+    text_model = GooglePalm(
+                model=text_model["model_name"],
+                temperature=text_model["parameters"]["temperature"],
+                max_output_tokens=text_model["parameters"]["max_output_tokens"],
+                top_k=text_model["parameters"]["top_k"],
+                top_p=text_model["parameters"]["top_p"],
+                google_api_key=GOOGLE_API_KEY
+            )
+    
+    chat_model = GooglePalm(
+                model=chat_model["model_name"],
+                temperature=chat_model["parameters"]["temperature"],
+                max_output_tokens=chat_model["parameters"]["max_output_tokens"],
+                top_k=chat_model["parameters"]["top_k"],
+                top_p=chat_model["parameters"]["top_p"],
+                google_api_key=GOOGLE_API_KEY
+            )
+    
+    embedding_model = GooglePalm(
+            model=embedding_model["model_name"],
+            temperature=embedding_model["parameters"]["temperature"],
+            max_output_tokens=embedding_model["parameters"]["max_output_tokens"],
+            top_k=embedding_model["parameters"]["top_k"],
+            top_p=embedding_model["parameters"]["top_p"],
+            google_api_key=GOOGLE_API_KEY
+        )
---
+++ backend/llm/llm_base/prompt.py
@@ -0,0 +1,58 @@
+from langchain.prompts import PromptTemplate, FewShotPromptTemplate
+from langchain.chains import LLMChain
+from llm_base.palm_model import PalmModel
+
+class Prompt:
+
+    prompt: PromptTemplate
+
+    def __init__(self) -> None:
+        pass
+
+    def get_result_with_text_model(self) -> str:
+        llm = PalmModel.text_model
+        chain = LLMChain(llm=llm, prompt=self.prompt)
+        response = chain.run({})
+
+        return response
+    
+    def get_result_with_chat_model(self) -> str:
+        llm = PalmModel.chat_model
+        chain = LLMChain(llm=llm, prompt=self.prompt)
+        response = chain.run({})
+
+        return response
+        
+
+class FewShotPrompt(Prompt):
+
+    def __init__(self, config, input: str) -> PromptTemplate:
+        example_prompt = PromptTemplate(
+            input_variables=config["example_input_variables"],
+            template=config["example_template"]
+        )
+
+        few_shot_prompt = FewShotPromptTemplate(
+            examples=config["examples"], 
+            example_prompt=example_prompt, 
+            prefix="\n".join([ config["context"], config["prefix"] ]),
+            suffix=config["suffix"], 
+            input_variables=[ config["actual_input_variable"] ],
+        )
+
+        formatted_prompt = few_shot_prompt.format(**{ config["actual_input_variable"]: input })
+
+        self.prompt = PromptTemplate(template=formatted_prompt, input_variables=[])
+    
+    
+class BasicPrompt(Prompt):
+
+    def __init__(self, config, input: str) -> PromptTemplate:
+        prompt = PromptTemplate(
+            input_variables=[ config["input_variable"] ],
+            template=config["template"]
+        )
+
+        formatted_prompt = prompt.format(**{ config["input_variable"]: input })
+
+        self.prompt = PromptTemplate(template=formatted_prompt, input_variables=[])
