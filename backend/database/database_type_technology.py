ff01b899311f6e8ebd852bc93fd9f03aa04617c8
tienthegainz <viettienha98@gmail.com>
2023-10-15 11:03:29 +0900

    #30 fix: update database retrieval logic

--- backend/database/database_type_technology.py
+++ backend/database/database_type_technology.py
@@ -1,8 +1,6 @@
-import json
-import os
 from typing import List
 
-from database.database_type_technologies import all_data as all_database_type_technologies
+from database.database_type_technologies import all_data
 
 
 def remove_question_key(raw_db_attributes: dict) -> dict:
@@ -11,19 +9,22 @@ def remove_question_key(raw_db_attributes: dict) -> dict:
     for key, attr in raw_db_attributes.items():
         if not isinstance(attr, dict):
             db_attributes[key] = attr
-        else:
+            continue
+        if "answer" in attr:
             db_attributes[key] = attr["answer"]
+        else:
+            db_attributes[key] = remove_question_key(attr)
     return db_attributes
 
 
-def load_database_type_technologies() -> List[dict]:
+def load_database_type_technologies(all_technologies) -> List[dict]:
     # TODO: make dataclasses ???
     technologies = [
         remove_question_key(database_type_technology)
-        for database_type_technology in all_database_type_technologies
+        for database_type_technology in all_technologies
     ]
     technologies.sort(key=lambda tech: tech["index"])
     return technologies
 
 
-db_database_type_technologies = load_database_type_technologies()
+db_database_type_technologies = load_database_type_technologies(all_data)
