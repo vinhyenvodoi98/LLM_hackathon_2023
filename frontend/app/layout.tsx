2877cf90f1d8a1fb5739b9e8b392b7350085ca2d
vinhyenvodoi98 <vinhyenvodoi98@gmail.com>
2023-10-15 21:29:04 +0900

    feat: update frontend docker

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,9 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import Header from './components/Header'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -17,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Header />
         <div className="min-h-main bg-background">
           {children}
