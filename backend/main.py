ff98ac2015e0dde0cb1304a96942a870cd6bd953
Nhi.Nguyen <nhi.nguyen@access-company.com>
2023-10-16 23:01:30 +0900

    #27 feat: integrate llm into analysis API
    * Add GOOGLE_API_KEY to mock env file
    
    * Update template class and prompt config
    
    * Integrate LLM to backend to analyze requirement
    
    * #27 fix: fix server bug
    
    ---------
    
    Co-authored-by: tienthegainz <viettienha98@gmail.com>

--- backend/main.py
+++ backend/main.py
@@ -3,5 +3,4 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
-    os.environ["BACKEND_DATA"] = "MOCK"
     uvicorn.run("app:app", host="0.0.0.0", port=8080, reload=True)
--- backend/routes/analysis.py
+++ backend/routes/analysis.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import time
+from llm.llm_interactor import get_requirement_analysis_result
 
 from fastapi import APIRouter
 
@@ -33,7 +34,7 @@ def analysis_project_requirements(
         raise NotImplementedError("App only support db analysis for now.")
     logging.info("Database analysis process start")
     start_time = time.perf_counter()
-    if os.getenv('BACKEND_DATA') == "MOCK":
+    if os.environ.get('BACKEND_DATA') == "MOCK":
         result = APIDatabaseAnalysisResponseModel(
             data_model=DatabaseAnalysisDataModel(
                 data_type=DatabaseAnalysisDataType(
@@ -53,12 +54,7 @@ def analysis_project_requirements(
                     question="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
                     explanation="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
                     value=True,
-                ),
-                relationship_centric=BoolWithDescription(
-                    question="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
-                    explanation="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
-                    value=True,
-                ),
+                )
             ),
             requirements=DatabaseAnalysisRequirements(
                 volume=DatabaseAnalysisVolume(
@@ -66,7 +62,7 @@ def analysis_project_requirements(
                     explanation="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
                     value=[DatabaseAnalysisVolumeEnum.SMALL],
                 ),
-                complex_query_patterns=BoolWithDescription(
+                fast_response_time=BoolWithDescription(
                     question="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
                     explanation="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
                     value=False,
@@ -81,11 +77,6 @@ def analysis_project_requirements(
                     explanation="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
                     value=False,
                 ),
-                high_availability=BoolWithDescription(
-                    question="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
-                    explanation="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
-                    value=False,
-                ),
             ),
             cost=DatabaseAnalysisCost(
                 commercial_allow=BoolWithDescription(
@@ -96,8 +87,56 @@ def analysis_project_requirements(
             )
         )
     else:
-        # TODO: implement analysis here
-        raise NotImplementedError()
+        analysis_result = get_requirement_analysis_result(post_request.requirement)
+        result = APIDatabaseAnalysisResponseModel(
+            data_model=DatabaseAnalysisDataModel(
+                data_type=DatabaseAnalysisDataType(
+                    question="Which type of data that you want to save into your DB?",
+                    explanation=analysis_result["data_model"]["data_type"]["reason"],
+                    value=analysis_result["data_model"]["data_type"]["value"]
+                ),
+                unstructured_data=BoolWithDescription(
+                    question="Do your project contain unstructured data?",
+                    explanation=analysis_result["data_model"]["unstructured_data"]["reason"],
+                    value=analysis_result["data_model"]["unstructured_data"]["value"],
+                ),
+                time_series=BoolWithDescription(
+                    question="Time series data is a collection of observations obtained through repeated measurements over time. Is your data time series data?",
+                    explanation=analysis_result["data_model"]["time_series"]["reason"],
+                    value=analysis_result["data_model"]["time_series"]["value"],
+                )
+            ),
+            requirements=DatabaseAnalysisRequirements(
+                volume=DatabaseAnalysisVolume(
+                    question="How much data do you need to store? Less than 1 million records is small. Within 1 million and 100 million is medium. And above 100 million records is large.",
+                    explanation=analysis_result["requirements"]["volume"]["reason"],
+                    value=DatabaseAnalysisVolume.get_values(analysis_result["requirements"]["volume"]["value"]),
+                ),
+                fast_response_time=BoolWithDescription(
+                    question="Do your project require microsecond response time from database? For example, leaderboards or session store may requires it.",
+                    explanation=analysis_result["requirements"]["fast_response_time"]["reason"],
+                    value=analysis_result["requirements"]["fast_response_time"]["value"],
+                ),
+                read_consistency=BoolWithDescription(
+                    question="Is read consistency (any read request will return the most recent write) one of the most important requirements?",
+                    explanation=analysis_result["requirements"]["read_consistency"]["reason"],
+                    value=analysis_result["requirements"]["read_consistency"]["value"],
+                ),
+                high_write_workloads=BoolWithDescription(
+                    question="Do you have a high-write workloads?",
+                    explanation=analysis_result["requirements"]["high_write_workloads"]["reason"],
+                    value=analysis_result["requirements"]["high_write_workloads"]["value"],
+                ),
+            ),
+            cost=DatabaseAnalysisCost(
+                commercial_allow=BoolWithDescription(
+                    question="Are you willing to use commercial database?",
+                    explanation=analysis_result["cost"]["commercial_allow"]["reason"],
+                    value=analysis_result["cost"]["commercial_allow"]["value"],
+                ),
+            )
+        )
+
     end_time = time.perf_counter()
     logging.info(
         "Database analysis process end after {0:.2f}s".format(end_time-start_time)
