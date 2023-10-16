f9a5c96082ea9472a833c12ef69044a54e8f3ff2
vinhyenvodoi98 <vinhyenvodoi98@gmail.com>
2023-10-16 09:24:55 +0900

    feat: update init database type init

--- backend/database/database_type_technologies/__init__.py
+++ backend/database/database_type_technologies/__init__.py
@@ -6,12 +6,22 @@ from database.database_type_technologies.microsoft_sql_server import microsoft_s
 from database.database_type_technologies.mysql import mysql
 from database.database_type_technologies.postgresql import postgresql
 from database.database_type_technologies.tidb import tidb
-from database.database_type_technologies.timescaledb import timescaledb
+from database.database_type_technologies.mongodb import mongodb
+from database.database_type_technologies.dyamondb import dyamondb
+from database.database_type_technologies.cassandra import cassandra
+from database.database_type_technologies.elasticache import elasticache
+from database.database_type_technologies.cloud_memorystore import cloud_memorystore
+from database.database_type_technologies.redis import redis
+from database.database_type_technologies.timestream import timestream
+from database.database_type_technologies.bigtable import bigtable
+from database.database_type_technologies.influxdb import influxdb
 
 all_data = [
     mysql, postgresql, tidb,
     mariadb, timescaledb, microsoft_sql_server,
-    amazon_aurora, cloud_sql, firestore
+    amazon_aurora, cloud_sql, firestore, mongodb,
+    dyamondb, cassandra, elasticache, cloud_memorystore,
+    redis, timestream, bigtable, influxdb
 ]
 
 general_purpose_sql = [
@@ -21,11 +31,12 @@ general_purpose_sql = [
 ]
 
 general_purpose_no_sql = [
-    firestore
+    firestore, mongodb, dyamondb, elasticache,
+    cloud_memorystorem, redis
 ]
 
 time_series = [
-    timescaledb
+    timescaledb, cassandra, timestream, bigtable, influxdb
 ]
 
 in_memory = [
